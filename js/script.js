32a7182af4e2291aeb11aa2a45aa3750dcac80fe
Tonyman95 <115559488+Tonyman95@users.noreply.github.com>
2024-06-24 07:36:34 -0400

    Act2

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {})
+
+$("#ElegirPelicula").click(()=>{
+    $.ajax({
+        url: 'https://www.codigo-alfa.cl/aglo/Tester/peliculaAleatoria',
+        method: 'GET',
+        success:function(data){
+            console.log(data)
+            $("#datosPeliculas").html(
+                "Titulo: " + data.pelicula.title+"<br>"+
+                "Año: "+ data.pelicula.year)
+            console.log(data.pelicula.title)
+        }
+    })
+
+})
+
+function getPeliculas(){
+    $.ajax({
+        url:'https://www.codigo-alfa.cl/aglo/Tester/listasPeliculas',
+        method: 'GET',
+        success: function(data){
+            console.log(data)
+            const peliculasList = data.peliculas;
+            let cadenaOption = '<option>Lista de Peliculas</option>'
+            peliculasList.map((data,index)=>{
+                console.log(data,index)
+                console.log(peliculasList[index])
+                cadenaOption+= '<option val='+data.id+'>'+data.id+'  '+data.title+'</option>';
+            })
+            $("#opcionesPeliculas").html(cadenaOption);
+
+        }
+    })
+}
+
+function getDataPeliculas(valorSeleccionado){
+     console.log(valorSeleccionado)
+     let numero = parseInt(valorSeleccionado)
+    $.ajax({
+        url: 'https://www.codigo-alfa.cl/aglo/Tester/listasPeliculas',
+        method: 'GET',
+        success: function(data){
+            const peliculasList = data.peliculas;
+            $("#datoPeliculas").html("<p>Titulo: "+peliculasList[numero -1].title+"<br>"+"Año: "+peliculasList[numero-1].year+"<br>"+"Genero: "+peliculasList[numero-1].genre+"</p>")
+            console.log(peliculasList[numero -1].title)
+        }
+    })
+}
+
+getPeliculas()
+
